8bddbd25a546f477bfbbadb429063ef307fbb1b8
Milad244 <milad.abdi244@gmail.com>
2025-01-07 18:09:29 -0800

    Finished module 1.3

--- Module 1 Assignment/com.Milad.module1.3/src/Main.java
+++ Module 1 Assignment/com.Milad.module1.3/src/Main.java
@@ -6,11 +6,19 @@ public class Main {
 
         twoDShapes.add(new Circle(5)); //Area should be approximately 78.5
         twoDShapes.add(new Triangle(5, 5, 5)); //Area should be approximately 10.8
-        twoDShapes.add(new Triangle(50, 50)); //Area should be 1250
+        twoDShapes.add(new Triangle(50, 50)); //Area should be approximately 1250
 
         for (TwoDShape s : twoDShapes){
             System.out.println(s);
             System.out.println(s.getArea());
+
+            if (s instanceof Triangle){
+                Triangle triangle = (Triangle) s;
+                triangle.rotate90();
+                triangle.rotate180();
+                triangle.rotate(100);
+                System.out.println("We have rotated 370 degrees!");
+            }
         }
     }
 }
---
+++ Module 1 Assignment/com.Milad.module1.3/src/Rotate.java
@@ -0,0 +1,6 @@
+public interface Rotate {
+    // These methods will be forced to be overridden in Triangle when I implement this interface in Triangle
+    void rotate90();
+    void rotate180();
+    void rotate(double degrees);
+}
